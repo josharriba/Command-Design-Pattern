d83926d81fc292da467b3b620c570620c5377cec
joshy <joshjarriba@gmail.com>
2020-10-09 18:47:27 -0400

    2nd commit

---
+++ Command Design Pattern/src/BuyStockCommand.java
@@ -0,0 +1,14 @@
+
+public class BuyStockCommand {
+
+	String stockName;
+	int shares;
+	
+	public BuyStockCommand(StockTrade stockTrade, String stockName, int shares) {
+		
+	}
+	
+	public void execute() {
+		
+	}
+}
